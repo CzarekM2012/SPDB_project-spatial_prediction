bdc51cff183ac514d034b7a315f3a19af7cddda5
CzarekM2012 <czarekmoczulski@gmail.com>
2023-12-27 14:16:50 -0500

    Add option to pass the seed for random subsampling

--- src/util/data.py
+++ src/util/data.py
@@ -2,16 +2,15 @@ import math
 import numpy as np
 
 
-def subsample(data: np.ndarray, ratio: float):
+def subsample(data: np.ndarray, ratio: float, seed: int = None):
     if not 0 < ratio < 1:
         raise ValueError("Ratio needs to be between 0 and 1")
     subsampled = np.full_like(data, np.nan)
-    max_row_index = data.shape[0] - 1
-    max_col_index = data.shape[1] - 1
     samples_count = math.floor(data.size * ratio)
+    rng = np.random.default_rng(seed)
     while samples_count > 0:
-        x = np.random.randint(0, max_col_index)
-        y = np.random.randint(0, max_row_index)
+        x = rng.integers(0, data.shape[1])  # data.shape[1] - number of columns
+        y = rng.integers(0, data.shape[0])  # data.shape[0] - number of rows
         if np.isnan(subsampled[y, x]):
             subsampled[y, x] = data[y, x]
             samples_count -= 1
