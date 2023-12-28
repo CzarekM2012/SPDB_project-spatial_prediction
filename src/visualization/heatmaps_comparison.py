b8b45d8f5930467ecb83ae0e205965bc055c08eb
CzarekM2012 <czarekmoczulski@gmail.com>
2023-12-28 12:02:42 -0500

    Remove generating predictions in-place from visualisation script

--- src/visualization/heatmaps_comparison.py
+++ src/visualization/heatmaps_comparison.py
@@ -3,47 +3,54 @@ Generates heatmaps of data, predicted values and their difference
 """
 import argparse
 from pathlib import Path
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from src.util.data_io import read_data, write_data
-from src.util.data import subsample
+from src.util.data_io import read_data
+
+
+_TARGET_SUFFIX = "_comparison"
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="""Predict missing values in data using choosen model and generate graphical
-        representations"""
+        description="""Generate graphical comparison of data"""
     )
     parser.add_argument(
         "-r",
-        "--ref",
+        "--reference",
         required=True,
         type=Path,
-        help="File containing reference data to compare predictions with.",
-    )
-    parser.add_argument(
-        "-d",
-        "--data",
-        type=Path,
-        default=None,
-        help="""File containing data points to be used for prediction. Will be randomly subsampled
-        from reference data if omitted.""",
-    )
-    parser.add_argument(
-        "-m",
-        "--model",
-        required=True,
-        type=Path,
-        help="File containing model to be used for predictions",
+        help="File containing reference data to compare predictions with",
     )
     parser.add_argument(
         "-s",
-        "--save-name",
-        dest="save_name",
+        "--samples",
+        required=True,
         type=Path,
-        default="heatmaps",
-        help="""Name of files that results will be saved in. Files will be created in appropriate
-        subdirectories of `reports` top-level directory""",
+        help="""File containing data samples on the basis of which predictions were made""",
+    )
+    parser.add_argument(
+        "-p",
+        "--predictions",
+        required=True,
+        type=Path,
+        help="File containing predictions",
+    )
+    parser.add_argument(
+        "-t",
+        "--target",
+        type=Path,
+        default=None,
+        help=f"""File to save figure in. If omitted it will be saved in 'reports/figures'
+        directory '<reference>_<predictions>{_TARGET_SUFFIX}""",
+    )
+    parser.add_argument(
+        "-d",
+        "--display",
+        action="store_true",
+        default=False,
+        help="Display generated figure",
     )
     return parser.parse_args()
 
@@ -57,23 +64,18 @@ def show_heatmap(ax, data: np.ndarray, title: str, colormap: str = "jet"):
 
 if __name__ == "__main__":
     FIGURES_DIR = Path("reports/figures")
-    PREDICTIONS_DIR = Path("reports/predictions")
 
     args = parse_args()
-    ref_data = read_data(args.ref)
-    if args.data is None:
-        samples = subsample(ref_data, 0.02)
-    else:
-        samples = read_data(args.data)
+    ref_data = read_data(args.reference)
+    samples = read_data(args.samples)
+    predictions = read_data(args.predictions)
+    if args.target is None:
+        args.target = FIGURES_DIR / Path(
+            f"{args.reference.stem}_{args.predictions.stem}{_TARGET_SUFFIX}"
+        )
+    args.target = args.target.with_suffix(".png")
 
-    # args.model
-    temp_predicted = (
-        np.ones_like(ref_data) * ref_data.mean()
-    )  # TODO: Load model and use it for prediction
-    predictions_filename = args.save_name.with_suffix(".asc")
-    write_data(PREDICTIONS_DIR.joinpath(predictions_filename), temp_predicted)
-
-    difference = temp_predicted - ref_data
+    difference = predictions - ref_data
 
     fig, axs = plt.subplots(nrows=2, ncols=2)
     fig.tight_layout()
@@ -85,9 +87,8 @@ if __name__ == "__main__":
     plot = axs[0][0].scatter(x, y, s=0.1, c=samples[x, y], cmap="jet")
     plt.colorbar(plot)
     show_heatmap(axs[1][0], ref_data, "Reference data")
-    show_heatmap(axs[0][1], temp_predicted, "Predicted data")
+    show_heatmap(axs[0][1], predictions, "Predicted data")
     show_heatmap(axs[1][1], difference, "Difference", "bwr")
 
-    figure_filename = args.save_name.with_suffix(".png")
-    plt.savefig(FIGURES_DIR.joinpath(figure_filename), bbox_inches="tight")
+    plt.savefig(args.target, bbox_inches="tight")
     plt.show()
